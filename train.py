deec6e07e1cac09454cf66b1595078bd059a7629
bensonchow123 <email@bensonc.how>
2026-04-22 20:54:33 +0100

    chore: initalize project files

---
+++ train.py
@@ -0,0 +1,13 @@
+import numpy as np
+import PIL
+import pandas as pd
+import sklearn
+import scipy
+import seaborn as sns
+import torch
+import torchinfo
+import torchvision
+from tqdm import tqdm
+
+import matplotlib.pyplot as plt
+
