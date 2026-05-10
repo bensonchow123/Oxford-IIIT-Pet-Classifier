2f454e5952f23bf9b9ae4b8b20a9c41b9c1b3a9f
bensonchow123 <email@bensonc.how>
2026-05-10 20:52:05 +0100

    feat:model architecture defined, create train.py

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # Oxford-IIIT-Pet-Classifier - University of York machine learning coursework
+### For the marker
+If you are to check my progress history, please check the commit history for `train.ipynb` instead of the `train.py`
+
 ### The goal
-Given the Oxford-IIT-Pet-Classification dataset, create a from scratch convolution network with the highest testing accuracy in 30 epoches, no external data.
+Given the Oxford-IIT-Pet-Classification trainval dataset, create a from scratch convolution network with the highest testing accuracy in 30 epoches, no external data.
 
 ### Hardware/ methodology
-I have access to 16 1080ti in 2 servers, therefore I will run different training loops, see the `training_loops directory`, I will document what I have tried, in the `/train_loops/stuff_i_tried.txt`
+I have access to 15 1080ti in 2 servers, therefore I will run training loops concurrently, see the `training_loops directory`, I will document what I have tried, in the `/train_loops/stuff_i_tried.txt`
 Multiple training loops is ran concurrently accorss the GPUs on the 2 servers to get the highest testing accuracy on the testing dataset.  
-Each training loop will have slightly different parameters/ CNN architecture.  
-I prevent myself from using the testing results to edit my CNN, so only uses the validation accuracy to do determine what to change.
+Each training loop will have slightly different hyperparameters/ CNN architecture.  
+I prevent myself from using the testing results to edit my CNN, so only uses the validation accuracy to do determine what to change, to prevent the testing dataset leakage.
+My final model at `train.py` is without the valuation split for maximum training data for maximum accuracy, achieving 72.11% testing accuracy.
 
 ### Current best results
 My current best result CNN will be the one in the `train.ipynb` with the model saved at `pet_classifer_weights.pth`
-
---
+++ train.py
@@ -0,0 +1,212 @@
+import time
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader, Dataset
+from torchvision import datasets
+from torchvision.transforms import v2
+from torchvision.transforms.v2 import CutMix, MixUp
+from tqdm import tqdm
+
+# Pretrianing:
+# Training data transforms and augmentation
+training_tfms_and_agmt = v2.Compose([
+    v2.ToImage(), # this turns it into a tensor
+
+    v2.RandomRotation(degrees=15), # for realistic pet location variations 
+    v2.RandomAffine(degrees=10, translate=(0.1, 0.1)), # for better position accuracy
+    v2.RandomHorizontalFlip(p=0.5), # it is still the same cat if flip like this but double data
+
+    v2.RandomResizedCrop((224, 224), scale=(0.8, 1.0), antialias=True), # 224 * 224 seems to be what everyone doing
+    v2.ColorJitter(brightness=0.3, contrast=0.2, saturation=0.2, hue=0.05), # this is for pet photographic varience
+
+    v2.ToDtype(torch.float32, scale=True), # apply the min-max normalization and scale is for 0-255 to num between 0 and 1
+    v2.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]), # apply the standardization for the z-scores things
+])
+
+
+class TransformWrapper(Dataset):
+    def __init__(self, subset, transform=None):
+        super().__init__() # inherit the Dataset init method just in case even though I think there is nothing
+        self.subset = subset
+        self.transform = transform
+
+    def __getitem__(self, index):
+        x, y = self.subset[index]
+        if self.transform:
+            x = self.transform(x)
+        return x, y
+
+    def __len__(self):
+        return len(self.subset)
+
+# load the trainval dataset for training
+raw_data = datasets.OxfordIIITPet(root="/shared/storage/cs/studentscratch/cqh514", split='trainval', download=True)
+
+# apply transforms and create a dataset
+training_dataset = TransformWrapper(raw_data, transform=training_tfms_and_agmt)
+
+# shuffle the data and use 10 cpu cores
+training_loader = DataLoader(training_dataset, batch_size=32, shuffle=True, num_workers=10)
+
+# save the model weights to my 5G of storage
+model_save_path = "/home/userfs/c/cqh514/Documents/Oxford-IIIT-Pet-Classifier/pet_classifier_weights.pth"
+device = torch.device("cuda:7") # cuda:1 is gpu 1, cuda:2 is gpu 2, etc
+
+# Training:
+class SEResBlock(nn.Module): # Resblock but focus on specific channels
+    def __init__(self, channels, reduction=16):
+        super().__init__()
+        self.block = nn.Sequential(
+            nn.Conv2d(channels, channels, 3, padding=1),
+            nn.BatchNorm2d(channels),
+            nn.ReLU(),
+            nn.Conv2d(channels, channels, 3, padding=1),
+            nn.BatchNorm2d(channels),
+        )
+        self.se = nn.Sequential( # the se block 
+            nn.AdaptiveAvgPool2d(1),
+            nn.Flatten(),
+            nn.Linear(channels, channels // reduction),
+            nn.ReLU(),
+            nn.Linear(channels // reduction, channels),
+            nn.Sigmoid()
+        )
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        out = self.block(x)
+        scale = self.se(out).view(x.size(0), -1, 1, 1)
+        return self.relu(x + out * scale) # normal resblock + se block scale
+
+class OxfordPetClassifierCNN(nn.Module):
+    def __init__(self, num_classes=37):
+        super().__init__()
+        
+        # Convolutional Layers, adding more then 6 seems to decrease accuracy
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=7, padding=3),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            SEResBlock(64),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            SEResBlock(128),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            SEResBlock(256),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            SEResBlock(512),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(512, 1024, kernel_size=3, padding=1),
+            nn.BatchNorm2d(1024),
+            nn.ReLU(),
+            SEResBlock(1024),
+            nn.MaxPool2d(2, 2),
+        )
+        
+        # Only one linear Layer with AdaptiveAvgPool2d instead of multiple flatten 
+        self.classifier = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Flatten(),
+            nn.Linear(1024, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        output = self.classifier(x) # logits is the name for the numbers outputed before softmax turns it into probabilities
+        return output
+
+model = OxfordPetClassifierCNN().to(device)
+print(model)
+
+# use the cross entropy loss function with label smoothing, instead of like 1 or 0 for classification, make it less definitive
+criterion = nn.CrossEntropyLoss(label_smoothing=0.1)
+
+# AdamW optimizer for the loss function
+optimizer = torch.optim.AdamW(model.parameters(), weight_decay=1e-4)
+
+EPOCHS = 30
+NUM_CLASSES = 37
+MIXUP_OR_CUTMIX_START_EPOCH = 20
+
+# Tell the scheduler exactly how many total steps there are
+total_steps = len(training_loader) * EPOCHS
+
+# The one cycle Scheduler, basicly peak learning rate at epoch 15 and slow down by epoch 30, so it will be best for my 30 epoch limit
+scheduler = torch.optim.lr_scheduler.OneCycleLR(
+    optimizer,
+    max_lr=3e-3, # peak learning rate
+    total_steps=total_steps, # this is basicly 30
+    pct_start=0.3, # 30% is warm up 
+    div_factor=25.0, # div by this and you got the starting learning rate
+    final_div_factor=1000.0 # div by this and you got ending learning rate
+)
+
+cutmix = CutMix(num_classes=NUM_CLASSES, alpha=0.1)
+mixup = MixUp(num_classes=NUM_CLASSES, alpha=0.1)
+cutmix_or_mixup = v2.RandomChoice([cutmix, mixup])
+
+for epoch in range(EPOCHS):
+    print(f"Epoch {epoch+1}/{EPOCHS}")
+    print("-" * 15)
+    start_time = time.time() # for seeing how long a epoch takes and might just use another gpu
+    
+    # training starts here
+    model.train() # this basicly activates the dropout but i removed it now just normalize based on current batch's mean/varience
+    running_loss = 0.0 # rset stuff after each epoch
+    correct_train = 0
+    total_train = 0
+    
+    use_augmentation = epoch >= MIXUP_OR_CUTMIX_START_EPOCH
+    
+    for inputs, labels in tqdm(training_loader, desc="Training", leave=False): 
+        inputs, labels = inputs.to(device), labels.to(device) # move data to the vram so it faster
+        
+        if use_augmentation:
+            inputs, labels = cutmix_or_mixup(inputs, labels) # add cutmix_or_mixup
+        
+        optimizer.zero_grad() # clear gradient from last batch
+        outputs = model(inputs) # forward the data
+        loss = criterion(outputs, labels) # run loss function
+        loss.backward() # back probagation
+        
+        optimizer.step() # update the weights 
+        
+        scheduler.step() # update the learning rate for the one cycle lr
+        
+        running_loss += loss.item() * inputs.size(0) # loss per epoch
+        _, predicted = torch.max(outputs, 1) # get the prediciton no need the score
+        total_train += labels.size(0) # get total images
+        
+        if use_augmentation:
+            correct_train += (predicted == labels.argmax(dim=1)).sum().item() # add up the softlabels
+        else:
+            correct_train += (predicted == labels).sum().item() # count correct
+        
+    train_acc = correct_train / total_train # the training accuracy
+    current_lr = optimizer.param_groups[0]['lr'] # current learning rate
+    
+    print(f"LR: {current_lr:.5f} | Train Acc: {train_acc * 100:.2f}% | Time: {time.time() - start_time:.0f}s")
+    
+# save the model after all epochs finish
+torch.save(model.state_dict(), model_save_path)
+print(f"🌟 Saved final model at epoch {EPOCHS} with Train Acc: {train_acc * 100:.2f}%")
+print(f"\nTraining done!")
+
