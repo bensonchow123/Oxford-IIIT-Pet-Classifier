0dc492b32dbd845de0f982be01459f3e008afb72
bensonchow123 <email@bensonc.how>
2026-05-21 17:11:00 +0100

    chore: prepare for submission

--- test.py
+++ test.py
@@ -91,10 +91,10 @@ class OxfordPetClassifierCNN(nn.Module):
             SEResBlock(512),
             nn.MaxPool2d(2, 2),
 
-            nn.Conv2d(512, 1024, kernel_size=3, padding=1),
-            nn.BatchNorm2d(1024),
+            nn.Conv2d(512, 768, kernel_size=3, padding=1),
+            nn.BatchNorm2d(768),
             nn.ReLU(),
-            SEResBlock(1024),
+            SEResBlock(768),
             nn.MaxPool2d(2, 2),
         )
         
@@ -102,7 +102,7 @@ class OxfordPetClassifierCNN(nn.Module):
         self.classifier = nn.Sequential(
             nn.AdaptiveAvgPool2d((1, 1)), # make it into a single average number
             nn.Flatten(), # remove all the empty dimensions
-            nn.Linear(1024, num_classes) # map the 1024 features to 37 breed scores
+            nn.Linear(768, num_classes) # map the 1024 features to 37 breed scores
         )
 
     def forward(self, x):
@@ -110,46 +110,51 @@ class OxfordPetClassifierCNN(nn.Module):
         output = self.classifier(x) # 37 raw numbers for the possibilities
         return output
 
-model_save_path = "/home/userfs/c/cqh514/Documents/Oxford-IIIT-Pet-Classifier/pet_classifier_weights.pth"
-device = torch.device("cuda") 
+# this is needed for it to work on windows
+if __name__ == "__main__":
+    # changed so it will work with the .pth file in the zip file
+    model_save_path = "pet_classifier_weights.pth"
 
-test_tfms = v2.Compose([ # this needs to be same as the valuation transform
-    v2.ToImage(),
-    v2.Resize(256, antialias=True),
-    v2.CenterCrop(224),
-    v2.ToDtype(torch.float32, scale=True),
-    v2.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
-])
+    # Just in case the gpus in the cse/270 not work
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu") 
 
-# load the testing set
-raw_test_data = datasets.OxfordIIITPet(root="/shared/storage/cs/studentscratch/cqh514", split='test', download=True)
+    test_tfms = v2.Compose([ # this needs to be same as the valuation transform
+        v2.ToImage(),
+        v2.Resize(256, antialias=True),
+        v2.CenterCrop(224),
+        v2.ToDtype(torch.float32, scale=True),
+        v2.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    ])
 
-# create the testing dataloader
-test_dataset = TransformWrapper(raw_test_data, transform=test_tfms)
-test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False, num_workers=10)
+    # load the testing set, changed to . so it will work for the submission bash script
+    raw_test_data = datasets.OxfordIIITPet(root=".", split='test', download=True)
 
-# define model and load the weights
-model = OxfordPetClassifierCNN(num_classes=37).to(device)
-model.load_state_dict(torch.load(model_save_path, weights_only=False))
+    # create the testing dataloader
+    test_dataset = TransformWrapper(raw_test_data, transform=test_tfms)
+    test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False, num_workers=4)
 
-# set to evaulation mode disable the batch norm
-model.eval()
+    # define model and load the weights
+    model = OxfordPetClassifierCNN(num_classes=37).to(device)
+    model.load_state_dict(torch.load(model_save_path, map_location=device, weights_only=False))
 
-correct_test = 0
-total_test = 0
+    # set to evaulation mode disable the batch norm
+    model.eval()
 
-print("Starting evaluation on the test dataset...")
+    correct_test = 0
+    total_test = 0
 
-# disable gradient calculations for testing, basicly same code as the validation phase in the training loop
-with torch.no_grad():
-    for inputs, labels in tqdm(test_loader, desc="Testing"):
-        inputs, labels = inputs.to(device), labels.to(device)
-        
-        outputs = model(inputs)
-        _, predicted = torch.max(outputs, 1)
-        
-        total_test += labels.size(0)
-        correct_test += (predicted == labels).sum().item()
+    print("Starting evaluation on the test dataset...")
 
-test_acc = correct_test / total_test
-print(f"\nFinal Test Accuracy: {test_acc * 100:.2f}%")
+    # disable gradient calculations for testing, basicly same code as the validation phase in the training loop
+    with torch.no_grad():
+        for inputs, labels in tqdm(test_loader, desc="Testing"):
+            inputs, labels = inputs.to(device), labels.to(device)
+            
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs, 1)
+            
+            total_test += labels.size(0)
+            correct_test += (predicted == labels).sum().item()
+
+    test_acc = correct_test / total_test
+    print(f"\nFinal Test Accuracy: {test_acc * 100:.2f}%")
--- train.py
+++ train.py
@@ -40,18 +40,6 @@ class TransformWrapper(Dataset):
     def __len__(self):
         return len(self.subset)
 
-# load the trainval dataset for training
-raw_data = datasets.OxfordIIITPet(root="/shared/storage/cs/studentscratch/cqh514", split='trainval', download=True)
-
-# apply transforms and create a dataset
-training_dataset = TransformWrapper(raw_data, transform=training_tfms_and_agmt)
-
-# shuffle the data and use 10 cpu cores
-training_loader = DataLoader(training_dataset, batch_size=32, shuffle=True, num_workers=10)
-
-# save the model weights to my 5G of storage
-model_save_path = "/home/userfs/c/cqh514/Documents/Oxford-IIIT-Pet-Classifier/pet_classifier_weights.pth"
-device = torch.device("cuda") # cuda:1 is gpu 1, cuda:2 is gpu 2, etc
 
 # Training:
 # Resblock but focus on specific channels, residual connection fixes gradient vanquishing during backprobagation
@@ -122,10 +110,10 @@ class OxfordPetClassifierCNN(nn.Module):
             SEResBlock(512),
             nn.MaxPool2d(2, 2),
 
-            nn.Conv2d(512, 1024, kernel_size=3, padding=1),
-            nn.BatchNorm2d(1024),
+            nn.Conv2d(512, 768, kernel_size=3, padding=1),
+            nn.BatchNorm2d(768),
             nn.ReLU(),
-            SEResBlock(1024),
+            SEResBlock(768),
             nn.MaxPool2d(2, 2),
         )
         
@@ -133,7 +121,7 @@ class OxfordPetClassifierCNN(nn.Module):
         self.classifier = nn.Sequential(
             nn.AdaptiveAvgPool2d((1, 1)), # make it into a single average number
             nn.Flatten(), # remove all the empty dimensions
-            nn.Linear(1024, num_classes) # map the 1024 features to 37 breed scores
+            nn.Linear(768, num_classes) # map the 768 features to 37 breed scores
         )
 
     def forward(self, x):
@@ -141,85 +129,105 @@ class OxfordPetClassifierCNN(nn.Module):
         output = self.classifier(x) # 37 raw numbers for the possibilities
         return output
 
-model = OxfordPetClassifierCNN().to(device)
-print(model)
+# This is needed for data loaders using multiple cpu cores to work on windows
+if __name__ == "__main__":
+    # manual seed for more consistant results
+    torch.manual_seed(0)
 
-# use the cross entropy loss function with label smoothing, instead of like 1 or 0 for classification, have like 10 percent for other classes
-criterion = nn.CrossEntropyLoss(label_smoothing=0.1)
+    # load the trainval dataset for training
+    raw_data = datasets.OxfordIIITPet(root=".", split='trainval', download=True)
 
-# Adam optimizer for the loss function, weight decay prevents model relying on single feature
-optimizer = torch.optim.AdamW(model.parameters(), weight_decay=1e-4)
+    # apply transforms and create a dataset
+    training_dataset = TransformWrapper(raw_data, transform=training_tfms_and_agmt)
 
-EPOCHS = 30 # max amount of epoch i allowed to use
-NUM_CLASSES = 37 # num of breed of cat and dogs
-MIXUP_OR_CUTMIX_START_EPOCH = 20 # mixup and cutmix need to work on the batches, but starting from scratch lowers accuracy, 15 - 20 is best
+    # shuffle the data and use 10 cpu cores
+    training_loader = DataLoader(training_dataset, batch_size=32, shuffle=True, num_workers=4)
 
-# Tell the scheduler exactly how many total steps there are
-total_steps = len(training_loader) * EPOCHS
+    # save the model weights to my 5G of storage
+    model_save_path = "pet_classifier_weights.pth"
 
-# The one cycle Scheduler, basicly peak learning rate at epoch 9 and slow down to basiclly zero by epoch 30, so it will be best for my 30 epoch limit
-scheduler = torch.optim.lr_scheduler.OneCycleLR(
-    optimizer,
-    max_lr=3e-3, # peak learning rate
-    total_steps=total_steps, # the gradient steps for 30 epochs
-    pct_start=0.3, # 30% of the epoches is warm up 
-    div_factor=25.0, # div by this and you got the starting learning rate
-    final_div_factor=1000.0 # div peak lr by this and get ending learning rate
-)
+    # just in case the gpu not working in cse/270
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # cuda:1 is gpu 1, cuda:2 is gpu 2, etc
 
-# define the cutmix and mixup image augmentations, fix overfitting
-cutmix = CutMix(num_classes=NUM_CLASSES, alpha=0.1) # just enough to prevent overfitting, tested to be best
-mixup = MixUp(num_classes=NUM_CLASSES, alpha=0.1) # just enough to prevent overfitting, tested to be best
-cutmix_or_mixup = v2.RandomChoice([cutmix, mixup]) # use it randomly on batches to prevent overfitting
+    # move the model to the GPU
+    model = OxfordPetClassifierCNN().to(device)
 
-# training loop
-for epoch in range(EPOCHS):
-    print(f"Epoch {epoch+1}/{EPOCHS}")
-    print("-" * 15)
-    start_time = time.time() # for seeing how long a epoch takes and might just use another gpu
-    
-    # training starts here
-    model.train() # this basicly activates the dropout but i removed it now it just turn on and off the batch norm
-    # reset stuff after each epoch
-    running_loss = 0.0
-    correct_train = 0
-    total_train = 0
-    
-    use_augmentation = epoch >= MIXUP_OR_CUTMIX_START_EPOCH # determine to use the batch augmentations or not
-    
-    for inputs, labels in tqdm(training_loader, desc="Training", leave=False): # make it update instead of new line for the bar
-        inputs, labels = inputs.to(device), labels.to(device) # move data to the vram 
+    # use the cross entropy loss function with label smoothing, instead of like 1 or 0 for classification, have like 10 percent for other classes
+    criterion = nn.CrossEntropyLoss(label_smoothing=0.1)
+
+    # Adam optimizer for the loss function, weight decay prevents model relying on single feature
+    optimizer = torch.optim.AdamW(model.parameters(), weight_decay=1e-4)
+
+    EPOCHS = 30 # max amount of epoch i allowed to use
+    NUM_CLASSES = 37 # num of breed of cat and dogs
+    MIXUP_OR_CUTMIX_START_EPOCH = 20 # mixup and cutmix need to work on the batches, but starting from scratch lowers accuracy, 15 - 20 is best
+
+    # Tell the scheduler exactly how many total steps there are
+    total_steps = len(training_loader) * EPOCHS
+
+    # The one cycle Scheduler, basicly peak learning rate at epoch 9 and slow down to basiclly zero by epoch 30, so it will be best for my 30 epoch limit
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(
+        optimizer,
+        max_lr=3e-3, # peak learning rate
+        total_steps=total_steps, # the gradient steps for 30 epochs
+        pct_start=0.3, # 30% of the epoches is warm up
+        div_factor=25.0, # div by this and you got the starting learning rate
+        final_div_factor=1000.0 # div peak lr by this and get ending learning rate
+    )
+
+    # define the cutmix and mixup image augmentations, fix overfitting
+    cutmix = CutMix(num_classes=NUM_CLASSES, alpha=0.1) # just enough to prevent overfitting, tested to be best
+    mixup = MixUp(num_classes=NUM_CLASSES, alpha=0.1) # just enough to prevent overfitting, tested to be best
+    cutmix_or_mixup = v2.RandomChoice([cutmix, mixup]) # use it randomly on batches to prevent overfitting
+
+    # training loop
+    for epoch in range(EPOCHS):
+        print(f"Epoch {epoch+1}/{EPOCHS}")
+        print("-" * 15)
+        start_time = time.time() # for seeing how long a epoch takes and might just use another gpu
         
-        if use_augmentation:
-            inputs, labels = cutmix_or_mixup(inputs, labels) # add augmentations to batches
+        # training starts here
+        model.train() # this basicly activates the dropout but i removed it now it just turn on and off the batch norm
+        # reset stuff after each epoch
+        running_loss = 0.0
+        correct_train = 0
+        total_train = 0
         
-        optimizer.zero_grad() # clear gradient from last batch
-        outputs = model(inputs) # forward the data
-        loss = criterion(outputs, labels) # run loss function
-        loss.backward() # back probagation
+        use_augmentation = epoch >= MIXUP_OR_CUTMIX_START_EPOCH # determine to use the batch augmentations or not
         
-        optimizer.step() # update the weights 
+        for inputs, labels in tqdm(training_loader, desc="Training", leave=False): # make it update instead of new line for the bar
+            inputs, labels = inputs.to(device), labels.to(device) # move data to the vram 
+            
+            if use_augmentation:
+                inputs, labels = cutmix_or_mixup(inputs, labels) # add augmentations to batches
+            
+            optimizer.zero_grad() # clear gradient from last batch
+            outputs = model(inputs) # forward the data
+            loss = criterion(outputs, labels) # run loss function
+            loss.backward() # back probagation
+            
+            optimizer.step() # update the weights 
+            
+            scheduler.step() # update the learning rate for the one cycle lr
+            
+            running_loss += loss.item() * inputs.size(0) # loss per epoch
+            _, predicted = torch.max(outputs, 1) # get the prediciton, discard the score
+            total_train += labels.size(0) # get total images
+            
+            if use_augmentation:
+                correct_train += (predicted == labels.argmax(dim=1)).sum().item() # add up the softlabels for correct train
+            else:
+                correct_train += (predicted == labels).sum().item() # count correct predictions
+            
+        train_acc = correct_train / total_train # the training accuracy
+        current_lr = optimizer.param_groups[0]['lr'] # current learning rate
         
-        scheduler.step() # update the learning rate for the one cycle lr
+        print(f"LR: {current_lr:.5f} | Train Acc: {train_acc * 100:.2f}% | Time: {time.time() - start_time:.0f}s")
         
-        running_loss += loss.item() * inputs.size(0) # loss per epoch
-        _, predicted = torch.max(outputs, 1) # get the prediciton, discard the score
-        total_train += labels.size(0) # get total images
-        
-        if use_augmentation:
-            correct_train += (predicted == labels.argmax(dim=1)).sum().item() # add up the softlabels for correct train
-        else:
-            correct_train += (predicted == labels).sum().item() # count correct predictions
-        
-    train_acc = correct_train / total_train # the training accuracy
-    current_lr = optimizer.param_groups[0]['lr'] # current learning rate
-    
-    print(f"LR: {current_lr:.5f} | Train Acc: {train_acc * 100:.2f}% | Time: {time.time() - start_time:.0f}s")
-    
-# save the model after all epochs finish
-torch.save(model.state_dict(), model_save_path)
+    # save the model after all epochs finish
+    torch.save(model.state_dict(), model_save_path)
 
-# signal that training is done
-print(f"🌟 Saved final model at epoch {EPOCHS} with Train Acc: {train_acc * 100:.2f}%")
-print(f"\nTraining done!")
+    # signal that training is done
+    print(f"Saved final model at epoch {EPOCHS} with Train Acc: {train_acc * 100:.2f}%")
+    print(f"\nTraining done!") 
 
